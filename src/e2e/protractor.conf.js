d03796c68dba0bf410342d1d021569fe77485032
heroined <heroinedor@gmail.com>
2017-03-12 17:17:54 +0100

    TravisCI : switch to firefox instead of chrome and launch webdriver-manager update before ci

--- src/e2e/protractor.conf.js
+++ src/e2e/protractor.conf.js
@@ -2,7 +2,7 @@ exports.config = {
     seleniumAddress: 'http://localhost:4444/wd/hub',
     specs: ['todo-spec.js'],
     capabilities: {
-        browserName: 'chrome'
+        browserName: 'firefox'
     },
     baseUrl: 'http://localhost:8080',
     framework: 'jasmine'
