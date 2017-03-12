d657c7bbe4214963df414cbeea8d88587d87af0f
heroined <heroinedor@gmail.com>
2017-03-12 16:57:58 +0100

    TravisCI : try to change npm script launched (ci) after npm install, and switch to chrome instead of firefox

--- src/e2e/protractor.conf.js
+++ src/e2e/protractor.conf.js
@@ -2,7 +2,7 @@ exports.config = {
     seleniumAddress: 'http://localhost:4444/wd/hub',
     specs: ['todo-spec.js'],
     capabilities: {
-        browserName: 'firefox'
+        browserName: 'chrome'
     },
     baseUrl: 'http://localhost:8080',
     framework: 'jasmine'
